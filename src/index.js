3df608258b454cc78db4b2cec47af249454c6117
Nicolas Vitaterna <nvitaterna@users.noreply.github.com>
2025-11-10 11:08:50 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `Server is running on http://localhost:${process.env.PORT || 3000}`
+  );
+});
+
+app.get("/", (req, res) => {
+  res.json(process.env);
+});
